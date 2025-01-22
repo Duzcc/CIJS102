f8c8d3f9c30fb9db856117342c703321ae6307b7
TVĐuc <vduc31100@gmail.com>
2025-01-22 09:39:59 +0700

    code

---
+++ my-Lession3-app/src/components/Card.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+const Card = (props) => {
+  const { titleCard, desCard } = props;
+  return (
+    <div className="card">
+      <div className="titleCard">
+        <span>{titleCard}</span>
+        <span>icon</span>
+      </div>
+      <p>{desCard}</p>
+      {props.children}
+      <button>MindX School</button>
+    </div>
+  );
+};
+export default Card;
